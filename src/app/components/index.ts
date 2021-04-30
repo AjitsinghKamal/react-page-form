ca2c7cb798250715eeda85b4a07576a0b08ef8b9
AjitsinghKamal <ajitskamal@gmail.com>
2021-04-30 12:58:51 +0530

    update component named exports

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,6 @@
 export { default as Seo } from './seo/seo';
 export { default as HeaderDefault } from './header/header-default';
 export { default as Button } from './button/button';
+export { default as Input } from './input/input';
+export { default as Checkbox } from './input/input-checkbox';
+export { default as CheckboxGroup } from './input/input-checkbox-group';
