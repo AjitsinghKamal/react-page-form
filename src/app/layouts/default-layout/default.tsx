4ceaa7256846c566c8a663f93da6ea086eb03986
AjitsinghKamal <ajitskamal@gmail.com>
2021-05-09 18:19:22 +0530

    feat: add loader to route suspense

--- src/app/layouts/default-layout/default.tsx
+++ src/app/layouts/default-layout/default.tsx
@@ -6,6 +6,8 @@ import cx from 'classnames';
 import { QuestionairePage, AboutPage } from 'app/pages';
 import { HeaderDefault } from 'app/components';
 import css from './default.module.scss';
+import { ReactComponent as Loader } from 'src/assets/svgs/pulse.svg';
+
 //#endregion
 
 type Props = {};
@@ -14,7 +16,7 @@ function DefaultLayout({ className }: Props & HTMLAttributes<HTMLDivElement>) {
 	return (
 		<div className={cx(css.layout, className)}>
 			<HeaderDefault />
-			<Suspense fallback={<div></div>}>
+			<Suspense fallback={<Loader className="loader___centered" />}>
 				<Switch>
 					<Route
 						path={QuestionairePage.path}
